1b72ca559599e04389d13ea9bdace9c1f52c7980
sergeysavchenko <savhcenko.sergey.v@gmail.com>
2025-01-09 11:00:03 +0200

    Перша версія програми. Функціонала поки що - ноль...

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+programm_version='0.0.0.1'
+programm_author = '@syteser'
+
+print(f'************************************************')
+print(f'HELLO!!!')
+print(f'Версія програми - {programm_version}')
+print(f'author: {programm_author} 2025')
+print(f'************************************************')
+
 print ('my first commit')
 
 #after MERGE....
